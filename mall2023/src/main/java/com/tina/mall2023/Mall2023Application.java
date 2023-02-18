be48a3d485b533f046e5b7e6917fa14fcda093b4
葉凡君 <44292591+yeahyeah0606@users.noreply.github.com>
2023-02-18 15:28:56 +0800

    first mall commit

---
+++ mall2023/src/main/java/com/tina/mall2023/Mall2023Application.java
@@ -0,0 +1,13 @@
+package com.tina.mall2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mall2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mall2023Application.class, args);
+    }
+
+}
