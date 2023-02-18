0b8ca159447dd618976c56177ecf768529bbfffc
葉凡君 <44292591+yeahyeah0606@users.noreply.github.com>
2023-02-18 23:22:20 +0800

    商品功能-查詢功能(分頁limit & offset)

--- mall2023/src/main/java/com/tina/mall2023/controller/ProductController.java
+++ mall2023/src/main/java/com/tina/mall2023/controller/ProductController.java
@@ -8,11 +8,14 @@ import com.tina.mall2023.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.List;
-
+@Validated
 @RestController
 public class ProductController {
     @Autowired
@@ -24,12 +27,18 @@ public class ProductController {
             @RequestParam(required = false) String search,
             // 排序 sorting
             @RequestParam(defaultValue = "created_date") String orderBy,
-            @RequestParam(defaultValue = "desc") String sort ){
+            @RequestParam(defaultValue = "desc") String sort,
+            // 分頁 pagination
+            @RequestParam(defaultValue = "5") @Max(1000) @Min(0) Integer limit,
+            @RequestParam(defaultValue = "0")  @Min(0) Integer offset ){
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
+
         List<Product> productList = productService.getProducts(productQueryParams);
         return ResponseEntity.status(HttpStatus.OK).body(productList);
     }
--- mall2023/src/main/java/com/tina/mall2023/dao/impl/ProductDaoImpl.java
+++ mall2023/src/main/java/com/tina/mall2023/dao/impl/ProductDaoImpl.java
@@ -30,6 +30,7 @@ public class ProductDaoImpl implements ProductDao {
                 "from product where 1=1";
         Map<String, Object> map = new HashMap<>();
 
+        // 查詢條件
         if( productQueryParams.getCategory() != null ){
             sql = sql +"  AND  category=:category";
             map.put("category", productQueryParams.getCategory().name());
@@ -38,8 +39,13 @@ public class ProductDaoImpl implements ProductDao {
             sql = sql +"  AND product_name LIKE :search";
             map.put("search", "%" + productQueryParams.getSearch() +"%");
         }
+        // 排序
         sql = sql + " ORDER BY " + productQueryParams.getOrderBy() + " " +
                 productQueryParams.getSort() ;
+        // 分頁
+        sql = sql + " LIMIT :limit OFFSET :offset";
+        map.put("limit", productQueryParams.getLimit());
+        map.put("offset", productQueryParams.getOffset());
 
         List<Product> productList = npjt.query(sql, map, new ProductRowMapper());
         return productList;
